30e83c53e2679bffbcadabc750fc433e7cca48f4
pipebotero <afbf492@gmail.com>
2025-01-08 00:33:46 -0500

    workflows config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  site: "http://pipebotero.github.io",
+  base: "fenix-escorts-web"
 });
