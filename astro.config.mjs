ef103ad19093e06826ab77eeff11da6805030765
Felipe Botero <afbf492@gmail.com>
2025-01-25 00:25:18 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  base: "fenix-escorts-web",
+  // base: "fenix-escorts-web",
   adapter: cloudflare(),
 });
