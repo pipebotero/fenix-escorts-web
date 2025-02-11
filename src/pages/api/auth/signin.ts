00ff2a4f51c9306fc859bedec7d2b0115010942f
Felipe Botero <afbf492@gmail.com>
2025-02-10 22:50:50 -0500

    Supabase integration

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -4,23 +4,19 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+  const req = await request.json();
+  const email = req.email.toString();
+  const password = req.password.toString();
 
   if (!email || !password) {
     return new Response("Correo electrónico y contraseña obligatorios", { status: 400 });
   }
-
-  console.log("Intentando autenticarse");
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
-
-  console.log({error});
   if (error) {
-    return new Response(error.message, { status: 500 });
+    return new Response(error.message, { status: error.status });
   }
 
   const { access_token, refresh_token } = data.session;
@@ -30,6 +26,5 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   cookies.set("sb-refresh-token", refresh_token, {
     path: "/",
   });
-  console.log("redireccionando");
-  return redirect("/dashboard");
+  return new Response("SignIn Success", { status: 200, statusText: "OK" });
 };
